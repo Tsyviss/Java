3addba05ad6bc5f8caca949c4360f6395773e6fd
Tsyviss <100566381+Tsyviss@users.noreply.github.com>
2023-09-23 08:33:53 +0300

    1.java

--- Tsyvis/1.java
+++ Tsyvis/1.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
+   public class Main{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Введите число : ");
-        int inNumber = input.nextInt();
-        if (inNumber > 7) {
+        int Number = input.nextInt();
+        if (Number > 7) {
             System.out.println("Привет");
-        } else if (inNumber == 7) {
+        } else if (Number == 7) {
             System.out.println("Введите число большее 7");
         } else {
             System.out.println("Данное число меньше 7");
         }
     }
+   }
